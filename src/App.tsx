3d3f1150b0c6a34d589e6ae959765ebc369a6c68
Cristiano Colangelo <cristiano.colangelo@bluewin.ch>
2022-10-14 10:29:24 +0200

    Continue fiddling with navbar

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,8 @@ import Navbar from "./components/Navbar";
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
+      <Navbar />
       <Grid minH="100vh" p={3}>
-        <Navbar />
-        <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
           <Logo h="40vmin" pointerEvents="none" />
           <Text>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,71 @@
-import {Box, Button, Flex, Text} from "@chakra-ui/react";
+import {Box, Button, Flex, flexbox, Text} from "@chakra-ui/react";
 import React, {PropsWithChildren} from "react";
 import logo from "../assets/ecoqube-logo-4C-highres.png";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import {ColorModeSwitcher} from "../ColorModeSwitcher";
+import EcoQubeLogo from "../assets/ecoqube-logo-4C-highres.png"
+import * as url from "url";
 
 type MenuItemProps = PropsWithChildren<{ isLast?: boolean, to: string }>;
-const MenuItem = ({ children, isLast, to = "/"}: MenuItemProps) => {
-  return (
-    <Text
-      mb={{ base: isLast ? 0 : 8, sm: 0 }}
-      mr={{ base: 0, sm: isLast ? 0 : 8 }}
-      display="block"
-    >
-      <Link to={to}>{children}</Link>
-    </Text>
-  );
+const MenuItem = ({children, isLast, to = "/"}: MenuItemProps) => {
+    return (
+        <Text
+            mb={{base: isLast ? 0 : 8, sm: 0}}
+            mr={{base: 0, sm: isLast ? 0 : 8}}
+            display="block"
+        >
+            <Link to={to}>{children}</Link>
+        </Text>
+    );
 };
 
+const divStyle = {
+    padding: "8px",
+    marginLeft: "50px",
+    display: "flex",
+    width: "100hw",
+    alignItems: "center",
+    backgroundImage: "url("+{EcoQubeLogo}+")"
+};
+
+const menuStyle = {
+    marginLeft: "auto",
+    marginRight: "50px"
+}
+
+const leftDivStyle = {
+    width: "40%",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center"
+
+}
+
+const textStyle = {
+    fontFamily: "Noto Sans"
+}
+
 const Navbar = () =>
-  <Flex
-    as="nav"
-    align="center"
-    justify="space-between"
-    wrap="wrap"
-    w="100%"
-    mb={8}
-    p={8}
-    bg={["primary.500", "primary.500", "transparent", "transparent"]}
-    color={["black", "black", "primary.700", "primary.700"]}
-  >
-    <p>Hey</p>
-  </Flex>
+    <div style={divStyle}>
+        <span style={leftDivStyle}>
+            <img width="110px" src={logo} alt="EcoQube logo"/>
+            <span style={textStyle}>Job Submission Platform</span>
+        </span>
+        <span style={menuStyle}>Menu</span>
+    </div>
+/*    <Flex
+        w="100%"
+        px="6"
+        py="5"
+        align="center"
+        justify="space-between"
+    >
+        <Flex>
+            <Box width="110px"><img src={logo} alt="EcoQube logo"/></Box>
+            <Text>Job Submission Platform</Text>
+        </Flex>
+        <Box justifySelf="flex-end">Menu</Box>
+        <ColorModeSwitcher justifySelf="flex-end"/>
+    </Flex>*/
 
 export default Navbar;
