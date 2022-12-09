1d8e7e492d904c84590ab43fa3e4e57f47c648e7
Cristiano Colangelo <cristiano@helio.exchange>
2022-12-09 17:42:52 +0100

    Add horizontal rulers

--- src/components/TargetChart.tsx
+++ src/components/TargetChart.tsx
@@ -3,6 +3,7 @@ import {AxisOptions, AxisTimeOptions, Chart} from "react-charts";
 import {useQuery} from "@tanstack/react-query";
 import axios from "axios";
 import {UserSerie} from "react-charts/types/types";
+import {Box, Spinner} from "@chakra-ui/react";
 
 type InstantCpuUsage = { timestamp: Date, data: number };
 
@@ -38,7 +39,7 @@ const TargetChart = () => {
     []
   );
 
-  const {data: payload, error, isLoading} = useQuery(["cpuUsage"], () =>
+  const {data: cpuUsagePayload, error, isLoading} = useQuery(["cpuUsage"], () =>
     axios
       .get("http://localhost:8080/api/v1/actualCpuUsageByRangeSeconds", { params:
           {start: subtractSeconds(new Date(), 10).toISOString(), end: new Date().toISOString()}
@@ -46,7 +47,16 @@ const TargetChart = () => {
       .then((res) => res.data)
   , {refetchInterval: 1000});
 
-  console.log(payload);
+  // TODO: Refactor this, i.e. don't repeat same code below (other is in TargetSelection), maybe get targets periodically
+  //  from a top-level components and update both TargetSelection and TargetChart (via props or context)
+  const {data: targetsPayload} = useQuery(["targets"], () =>
+    axios
+      .get("http://localhost:8080/api/v1/targets")
+      .then((res) =>
+        res.data
+      )
+  );
+
   const primaryAxis = React.useMemo(
     (): AxisOptions<InstantCpuUsage> => ({
       getValue: cpuUsage => cpuUsage.timestamp,
@@ -68,31 +78,46 @@ const TargetChart = () => {
         getValue: cpuUsage => cpuUsage.data,
         hardMin: 0,
         hardMax: 100,
-        scaleType: "linear"
+        scaleType: "linear",
       },
     ],
     []
   )
 
   useEffect(() => {
-    if (!payload) return;
+    if (!cpuUsagePayload) return;
 
-    const newData = payload.map((currentNodeData: NodeCpuUsageApiPayload) => ({
+    const newData = cpuUsagePayload.map((currentNodeData: NodeCpuUsageApiPayload) => ({
       label: currentNodeData.nodeName,
-      data: payload.find((u: NodeCpuUsageApiPayload) => u.nodeName == currentNodeData.nodeName)?.usage.map((elem: InstantCpuUsage) => ({
+      data: cpuUsagePayload.find((u: NodeCpuUsageApiPayload) => u.nodeName == currentNodeData.nodeName)?.usage.map((elem: InstantCpuUsage) => ({
         timestamp: new Date(elem.timestamp),
         data: elem.data
       }))
     }));
 
-    console.log(newData);
+    const rulers = Object.entries(targetsPayload.targets).map(([key, value]) => ({
+      label: key + "-target",
+      data: newData?.find((elem: UserSerie<NodeCpuUsageApiPayload>) => elem.label == key).data?.map((elem2: InstantCpuUsage) => ({
+        timestamp: new Date(elem2.timestamp),
+        data: value
+      }))
+    }))
+    newData.push(...rulers);
 
     // TODO: WHY!
     // @ts-ignore
     setGraphData(newData);
-  }, [payload]);
+  }, [cpuUsagePayload]);
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) {
+    return <Box display="flex" justifyContent="center" alignContent="center"><Spinner size='xl'/></Box>;
+  }
+
+  if (error) {
+    // TODO: Maybe investigate why error is of type unknown
+    // @ts-ignore
+    return <Box>Error: {error.message} 😱</Box>;
+  }
 
   return (
     <Chart
@@ -100,6 +125,7 @@ const TargetChart = () => {
         data: graphData,
         primaryAxis,
         secondaryAxes,
+        // defaultColors: ["#ff0000", "#00ffff", "#ff00ff", "#ff0000", "#00ffff", "#ff00ff"], // TODO: How to color code?
       }}
     />
   )
