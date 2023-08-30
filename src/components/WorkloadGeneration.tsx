92fa15665b445365c5c485dddfe33c135c6b3f6a
Cristiano Colangelo <cristiano@helio.exchange>
2023-08-30 11:27:30 +0200

    Add switches for schedulable and TAWA strategies, start file dropzone for scenario upload

--- src/components/WorkloadGeneration.tsx
+++ src/components/WorkloadGeneration.tsx
@@ -12,10 +12,20 @@ import {logDOM} from "@testing-library/react";
 import axios from "axios";
 import useDidMountEffect from "hooks/useDidMountEffect";
 import {MutableRefObject, useEffect, useRef, useState} from "react";
+import FileUploadForm from "./FileUpload";
 
-type SelfDrivingGetResponse = {
+type Enabled = {
   enabled: boolean;
 }
+
+type SelfDrivingGetResponse = {
+} & Enabled;
+
+type TawaGetResponse = {
+} & Enabled;
+
+type SchedulableGetResponse = {
+} & Enabled;
 
 const WorkloadsGeneration = () => {
   const toast = useToast();
@@ -87,6 +97,25 @@ const WorkloadsGeneration = () => {
     return axios.put('http://localhost:8080/api/v1/self-driving', selfDrivingMutationData)
   });
 
+  const [tawaIsChecked, setTawaIsChecked] = useState(false);
+  const [tawaSwitchDisabled, setTawaSwitchDisabled] = useState(true);
+  const [tawaMutationData, setTawaMutationData] = useState<TawaGetResponse | null>(null);
+
+  const switchTawaMode = useMutation(() => {
+    console.log("mutating...", selfDrivingMutationData);
+    return axios.put('http://localhost:8080/api/v1/self-driving', selfDrivingMutationData)
+  });
+
+  // Create switch for schedulable mode
+  const [schedulableIsChecked, setSchedulableIsChecked] = useState(false);
+  const [schedulableSwitchDisabled, setSchedulableSwitchDisabled] = useState(true);
+  const [schedulableMutationData, setSchedulableMutationData] = useState<SchedulableGetResponse | null>(null);
+
+  const switchSchedulableMode = useMutation(() => {
+    console.log("mutating...", schedulableMutationData);
+    return axios.put('http://localhost:8080/api/v1/schedulable', schedulableMutationData)
+  });
+
   const [initialRender, setInitialRender] = useState(true);
 
   const selfDrivingGetQuery = useQuery<SelfDrivingGetResponse, Error>(['self-driving-enabled'], () => {
@@ -105,12 +134,42 @@ const WorkloadsGeneration = () => {
     },
   });
 
-  useEffect(() => {
-    console.log("for some reason, isChecked is", selfDrivingIsChecked);
-  }, [selfDrivingIsChecked]);
+  const tawaGetQuery = useQuery<TawaGetResponse, Error>(['tawa-enabled'], () => {
+    return axios.get('http://localhost:8080/api/v1/tawa').then((res) => res.data);
+  }, {
+    enabled: false,
+    onSuccess: (data) => {
+      // Set the switch to the value of the response
+      console.log("setting switch to: ", data.enabled);
+      setTawaIsChecked(data?.enabled ?? false);
+      setTawaSwitchDisabled(false);
+    },
+    onError: (err) => {
+      setTawaIsChecked(false);
+      setTawaSwitchDisabled(false);
+    },
+  });
+
+  const schedulableGetQuery = useQuery<SchedulableGetResponse, Error>(['schedulable-enabled'], () => {
+    return axios.get('http://localhost:8080/api/v1/schedulable').then((res) => res.data);
+  }, {
+    enabled: false,
+    onSuccess: (data) => {
+      // Set the switch to the value of the response
+      console.log("setting switch to: ", data.enabled);
+      setSchedulableIsChecked(data?.enabled ?? false);
+      setSchedulableSwitchDisabled(false);
+    },
+    onError: (err) => {
+      setSchedulableIsChecked(false);
+      setSchedulableSwitchDisabled(false);
+    },
+  });
 
   useEffect(() => {
     selfDrivingGetQuery.refetch().then(r => setInitialRender(false));
+    tawaGetQuery.refetch().then(r => setInitialRender(false));
+    schedulableGetQuery.refetch().then(r => setInitialRender(false));
   }, []);
 
   useEffect(() => {
@@ -148,6 +207,76 @@ const WorkloadsGeneration = () => {
     }
   }, [selfDrivingMutationData]); // since user clicked the switch, selfDrivingMutationDAta will have changed, so the mutation needs to be fired
 
+  useEffect(() => {
+    // Don't mutate on initial render
+    if (!initialRender) {
+      switchTawaMode.mutateAsync()
+        .then((res) => {
+          if (res.data.message === "success") {
+            const title = tawaIsChecked ? 'TAWA mode enabled.' : 'TAWA mode disabled.';
+            toast({
+              title: title,
+              status: 'success',
+              duration: 2000,
+              isClosable: true,
+            });
+            // Since the mutation was successful, the switch can be enabled again
+            setTawaSwitchDisabled(false);
+            // Since the mutation was successful, the switch is set to the value of the mutation
+            setTawaIsChecked(tawaMutationData?.enabled ?? false);
+          }
+        })
+        .catch((err) => {
+          const title = tawaIsChecked ? 'enabling' : 'disabling';
+          setTawaIsChecked(false);
+          toast({
+            title: `Error ${title} TAWA mode.`,
+            status: 'warning',
+            duration: 2000,
+            isClosable: true,
+          });
+          console.log(err);
+          // User can try again, but value stays as before
+          setTawaSwitchDisabled(false);
+        });
+    }
+  }, [tawaMutationData]); // since user clicked the switch, selfDrivingMutationDAta will have changed, so the mutation needs to be fired
+
+  useEffect(() => {
+    // Don't mutate on initial render
+    if (!initialRender) {
+      switchSchedulableMode.mutateAsync()
+        .then((res) => {
+          if (res.data.message === "success") {
+            const title = schedulableIsChecked ? 'Schedulable mode enabled.' : 'Schedulable mode disabled.';
+            toast({
+              title: title,
+              status: 'success',
+              duration: 2000,
+              isClosable: true,
+            });
+            // Since the mutation was successful, the switch can be enabled again
+            setSchedulableSwitchDisabled(false);
+            // Since the mutation was successful, the switch is set to the value of the mutation
+            setSchedulableIsChecked(schedulableMutationData?.enabled ?? false);
+          }
+        })
+        .catch((err) => {
+          const title = schedulableIsChecked ? 'enabling' : 'disabling';
+          setSchedulableIsChecked(false);
+          toast({
+            title: `Error ${title} schedulable mode.`,
+            status: 'warning',
+            duration: 2000,
+            isClosable: true,
+          });
+          console.log(err);
+          // User can try again, but value stays as before
+          setSchedulableSwitchDisabled(false);
+        });
+    }
+  }, [schedulableMutationData]);
+
   return (<FormControl>
       <VStack spacing={5} align='stretch'>
         <FormControl display='flex' alignItems='center'>
@@ -159,6 +288,30 @@ const WorkloadsGeneration = () => {
             setSelfDrivingSwitchDisabled(true); // disable the switch until the mutation is done
             setSelfDrivingMutationData({enabled: !selfDrivingIsChecked}); // set the data to be sent to the mutation
             setSelfDrivingIsChecked(!selfDrivingIsChecked); // set the state of the switch to the opposite of the current state
+          }}
+          />
+        </FormControl>
+        <FormControl display='flex' alignItems='center'>
+          <FormLabel htmlFor='schedulable-mode' mb='0'>
+            Enable schedulable mode
+          </FormLabel>
+          <Switch id='schedulable-mode' isDisabled={schedulableSwitchDisabled} isChecked={schedulableIsChecked} onChange={() => {
+            console.log("current switch value", schedulableIsChecked);
+            setSchedulableSwitchDisabled(true); // disable the switch until the mutation is done
+            setSchedulableMutationData({enabled: !schedulableIsChecked}); // set the data to be sent to the mutation
+            setSchedulableIsChecked(!schedulableIsChecked); // set the state of the switch to the opposite of the current state
+          }}
+          />
+        </FormControl>
+        <FormControl display='flex' alignItems='center'>
+          <FormLabel htmlFor='tawa-mode' mb='0'>
+            Enable TAWA mode
+          </FormLabel>
+          <Switch id='tawa-mode' isDisabled={tawaSwitchDisabled} isChecked={tawaIsChecked} onChange={() => {
+            console.log("current switch value", tawaIsChecked);
+            setTawaSwitchDisabled(true); // disable the switch until the mutation is done
+            setTawaMutationData({enabled: !tawaIsChecked}); // set the data to be sent to the mutation
+            setTawaIsChecked(!tawaIsChecked); // set the state of the switch to the opposite of the current state
           }}
           />
         </FormControl>
@@ -207,6 +360,7 @@ const WorkloadsGeneration = () => {
           Clear all completed workloads
         </Button>
       </VStack>
+      {/*<FileUploadForm />*/}
     </FormControl>);
 }
 
